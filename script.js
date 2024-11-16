1887319c96f5f95bce264c9a9632468354325631
pilafnoodle <amberlin618@gmail.com>
2024-11-16 10:54:59 -0800

    update html

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const url=" https://tle.ivanstanojevic.me/api/tle"
+fetch('http://api.open-notify.org/iss-now.json')
+            .then(response => response.json())  // Convert the response to JSON
+            .then(data => {
+                // Access and log the timestamp and latitude values
+                console.log('Timestamp:', data.timestamp);
+                console.log('Latitude:', data.iss_position.latitude);
+                console.log('Longitude:', data.iss_position.longitude);
+                document.getElementById('sublong').textContent = data.iss_position.longitude;
+                document.getElementById('sublat').textContent = data.iss_position.latitude;
+                
+            })
+            .catch(error => {
+                // Handle any errors that occur during the fetch
+                console.error('Error fetching data:', error);
+            });
