1c01c3830ea95780b334ed7d91c1feaa06d414e4
Mac-Boyd <macboyd80@gmail.com>
2024-11-16 13:47:05 -0800

    displays results of 'can i see it'

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var satLoc = [];
 fetch('http://api.open-notify.org/iss-now.json')
             .then(response => response.json())  // Convert the response to JSON
             .then(data => {
@@ -5,6 +6,8 @@ fetch('http://api.open-notify.org/iss-now.json')
                 console.log('Timestamp:', data.timestamp);
                 console.log('Latitude:', data.iss_position.latitude);
                 console.log('Longitude:', data.iss_position.longitude);
+                satLoc[0] = data.iss_position.latitude;
+                satLoc[1] = data.iss_position.longitude;
                 document.getElementById('sublong').textContent = data.iss_position.longitude;
                 document.getElementById('sublat').textContent = data.iss_position.latitude;
                 
